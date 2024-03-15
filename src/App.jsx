4845b8c2615d93c9ba9100d74b60ae4dd15f8ecd
Md Noble <mdnoble.wd@gmail.com>
2024-03-15 21:33:22 +0600

    initialized react project and installed tailwind css and daisy ui

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-center text-6xl my-12 text-red-600">Vite + React</h1>
+      <button className="btn btn-info">Info</button>
+      <button className="btn btn-success">Success</button>
+      <button className="btn btn-warning">Warning</button>
+      <button className="btn btn-error">Error</button>
+    </>
+  );
+}
+
+export default App;
